6dab06ae978af8fa83bf44f963bba9611abd3e93
yangfengstat <84392512+r02pro@users.noreply.github.com>
2023-05-31 22:36:26 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# r02pro_source
+Source files for r02pro book
---
+++ index.R
@@ -0,0 +1,5 @@
+## ---- include=FALSE-------------------------------------------------
+knitr::opts_chunk$set(
+  comment = "#>", echo = TRUE, eval = TRUE, results = FALSE, error = TRUE, fig.width = 6, warning = FALSE
+)
+
